431ab61678f4134b65f15a9581f7841b6447d98a
shumaikunkun <s1711520@gmail.com>
2019-10-25 17:20:57 +0900

    しゅーてぃんぐの目標座標と出発座標を取得

---
+++ Assets/Scripts/Shumai/ShotScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour
+{
+    RaycastHit hit;
+    float time;
+    bool flag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        flag = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        //if()
+
+
+        //Debug.Log(time);
+        Shot();
+    }
+
+    void Shot()
+    {
+        Vector3 center = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        Ray ray = Camera.main.ScreenPointToRay(center);
+        float distance = 100;
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            Debug.Log("出発点" + transform.position);
+            Debug.Log("目標点" + hit.point);
+            //Instantiate(sparks, hit.point, Quaternion.identity);
+            //if (hit.collider.tag == "Enemy")
+            //{
+            //    hit.collider.SendMessage("Damage");
+            //}
+        }
+    }
+
+}
