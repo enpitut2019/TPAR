836a13668ad96b8d46a0aabd40f1ba3274a8bcce
shumaikunkun <s1711520@gmail.com>
2019-12-06 16:35:06 +0900

    winからカメラスクリプトをコピー

---
+++ Assets/Scripts/CameraControlForVR.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class CameraControlForVR : MonoBehaviour
+{
+    [SerializeField] Camera target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        XRDevice.DisableAutoXRCameraTracking(target, false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
